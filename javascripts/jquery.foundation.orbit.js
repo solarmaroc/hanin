fb1594b7e242adf595c1f79cb30a10b812c923d9
Hommad <swieppo@hotmail.com>
2016-10-13 15:52:09 +0200

    speed slider change

--- javascripts/jquery.foundation.orbit.js
+++ javascripts/jquery.foundation.orbit.js
@@ -21,9 +21,9 @@
 
     defaults: {
       animation: 'horizontal-push',     // fade, horizontal-slide, vertical-slide, horizontal-push, vertical-push
-      animationSpeed: 2500,        // how fast animtions are
+      animationSpeed: 1000,        // how fast animtions are
       timer: true,            // true or false to have the timer
-      advanceSpeed: 9000,         // if timer is enabled, time between transitions
+      advanceSpeed: 8000,         // if timer is enabled, time between transitions
       pauseOnHover: false,        // if you hover pauses the slider
       startClockOnMouseOut: false,    // if clock should start on MouseOut
       startClockOnMouseOutAfter: 1000,  // how long after MouseOut should the timer start again
